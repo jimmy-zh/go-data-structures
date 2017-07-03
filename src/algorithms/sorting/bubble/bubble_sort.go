c6d48fa23c0b79822c2da6785f6d3379cd29de78
midnight-vivian <midnight.vivian@gmail.com>
2017-07-03 18:10:56 +0800

    add bubble_sort

---
+++ src/algorithms/sorting/bubble/bubble_sort.go
@@ -0,0 +1,17 @@
+package bubble
+
+func Sort(arr []int) {
+	if arr == nil || len(arr) < 2 {
+		return
+	}
+	swap := true
+	for i := 0; i < len(arr)-1 && swap; i++ {
+		swap = false
+		for j := len(arr) - 1; j > i; j-- {
+			if arr[j] < arr[j-1] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
+				swap = true
+			}
+		}
+	}
+}
