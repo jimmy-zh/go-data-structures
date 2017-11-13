c57c77c225186830d2410bc8f962fb0cdb4e23f8
midnight-vivian <midnight.vivian@gmail.com>
2017-11-13 15:19:01 +0800

    optimize shell_sort

--- src/algorithms/sorting/shell/shell_sort.go
+++ src/algorithms/sorting/shell/shell_sort.go
@@ -4,24 +4,17 @@ func Sort(arr []int) {
 	if arr == nil || len(arr) < 2 {
 		return
 	}
-
 	h := 1
 	//find proper increment sequence
 	for h < len(arr)/3 {
 		h = h*3 + 1
 	}
-
 	for h >= 1 {
-
 		for i := h; i < len(arr); i++ {
-			for j := i; j >= h; j -= h {
-				if arr[j] < arr[j-h] {
-					arr[j], arr[j-h] = arr[j-h], arr[j]
-				}
+			for j := i; j >= h && arr[j] < arr[j-h]; j -= h {
+				arr[j], arr[j-h] = arr[j-h], arr[j]
 			}
 		}
-
 		h = h / 3
 	}
-
 }
