6796c8ddae4f8b0490224a1235517aaba4e141de
midnight-vivian <midnight.vivian@gmail.com>
2017-12-19 22:50:09 +0800

    add binary_tree_helper

---
+++ src/utils/binary_tree_helper.go
@@ -0,0 +1,75 @@
+package utils
+
+//Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func binaryTreeConstructor(vals []interface{}) *TreeNode {
+	if len(vals) == 0 {
+		return nil
+	}
+	nodes := make([]*TreeNode, 0)
+	parent := 0
+	left := true
+	node := (*TreeNode)(nil)
+	for i := range vals {
+		if val, ok := vals[i].(int); ok {
+			node = &TreeNode{Val: val}
+		} else {
+			node = nil
+		}
+		nodes = append(nodes, node)
+		if i == 0 {
+			continue
+		}
+		for parent < i && nodes[parent] == nil {
+			parent++
+		}
+		if parent == i {
+			//invalid input vals
+			return nil
+		}
+		if left {
+			nodes[parent].Left = node
+			left = false
+		} else {
+			nodes[parent].Right = node
+			left = true
+			parent++
+		}
+	}
+	/*
+		for _, node := range nodes {
+			if node == nil {
+				fmt.Println("null")
+			} else {
+				fmt.Println(node.Val)
+			}
+		}
+	*/
+	return nodes[0]
+}
+
+func binaryTreeEqual(t1, t2 *TreeNode) bool {
+	if t1 == nil {
+		if t2 == nil {
+			return true
+		}
+		return false
+	}
+	if t2 == nil {
+		return false
+	} else if t1.Val != t2.Val {
+		return false
+	}
+	if !binaryTreeEqual(t1.Left, t2.Left) {
+		return false
+	}
+	if !binaryTreeEqual(t1.Right, t2.Right) {
+		return false
+	}
+	return true
+}
